fce676867961d5e48949c4155fd4aef0803d63f5
Johannes Buchner <johannes.buchner.acad@gmx.com>
2016-07-12 19:44:41 +0200

    version bump to 2.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst') as f:
 
 setup(
     name='ImageHash',
-    version='2.1',
+    version='2.2',
     author='Johannes Buchner',
     author_email='buchner.johannes@gmx.at',
     packages=['imagehash'],
