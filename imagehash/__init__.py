4d937150955313e92a6beb53021c02cfc9c70a3d
Johannes Buchner <johannes.buchner.acad@gmx.com>
2016-10-28 20:00:42 -0300

    fixed formatting of previous commit

--- imagehash/__init__.py
+++ imagehash/__init__.py
@@ -92,16 +92,16 @@ def hex_to_hash(hexstr, hash_size=8):
 	Convert a stored hash (hex, as retrieved from str(Imagehash))
 	back to a Imagehash object.
 	"""
-        l = []
-        count = hash_size * (hash_size // 4)
-        if len(hexstr) != count:
-            emsg = 'Expected hex string size of {}.'
-            raise ValueError(emsg.format(count))
-        for i in range(count // 2):
-            h = hexstr[i*2:i*2+2]
-            v = int("0x" + h, 16)
-            l.append([v & 2**i > 0 for i in range(8)])
-        return ImageHash(numpy.array(l))
+	l = []
+	count = hash_size * (hash_size // 4)
+	if len(hexstr) != count:
+		emsg = 'Expected hex string size of {}.'
+		raise ValueError(emsg.format(count))
+	for i in range(count // 2):
+		h = hexstr[i*2:i*2+2]
+		v = int("0x" + h, 16)
+		l.append([v & 2**i > 0 for i in range(8)])
+	return ImageHash(numpy.array(l))
 
 
 def average_hash(image, hash_size=8):
