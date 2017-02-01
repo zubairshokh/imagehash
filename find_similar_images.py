32d3d9bea9b04125c933f39524233205924b3a5f
Johannes Buchner <johannes.buchner.acad@gmx.com>
2017-02-01 11:41:24 -0800

    fixed mixed indents in demo file

--- find_similar_images.py
+++ find_similar_images.py
@@ -11,25 +11,25 @@ Demo of hashing
 def find_similar_images(userpath, hashfunc = imagehash.average_hash):
     import os
     def is_image(filename):
-    	f = filename.lower()
-    	return f.endswith(".png") or f.endswith(".jpg") or \
-    		f.endswith(".jpeg") or f.endswith(".bmp") or f.endswith(".gif")
+        f = filename.lower()
+        return f.endswith(".png") or f.endswith(".jpg") or \
+            f.endswith(".jpeg") or f.endswith(".bmp") or f.endswith(".gif")
     
     image_filenames = [os.path.join(userpath, path) for path in os.listdir(userpath) if is_image(path)]
     images = {}
     for img in sorted(image_filenames):
-    	hash = hashfunc(Image.open(img))
-    	images[hash] = images.get(hash, []) + [img]
+        hash = hashfunc(Image.open(img))
+        images[hash] = images.get(hash, []) + [img]
     
     for k, img_list in six.iteritems(images):
-    	if len(img_list) > 1:
-    		print(" ".join(img_list))
+        if len(img_list) > 1:
+            print(" ".join(img_list))
 
 
 if __name__ == '__main__':
     import sys, os
     def usage():
-    	sys.stderr.write("""SYNOPSIS: %s [ahash|phash|dhash|...] [<directory>]
+        sys.stderr.write("""SYNOPSIS: %s [ahash|phash|dhash|...] [<directory>]
 
 Identifies similar images in the directory.
 
@@ -42,21 +42,21 @@ Method:
 
 (C) Johannes Buchner, 2013
 """ % sys.argv[0])
-    	sys.exit(1)
+         sys.exit(1)
     
     hashmethod = sys.argv[1] if len(sys.argv) > 1 else usage()
     if hashmethod == 'ahash':
-    	hashfunc = imagehash.average_hash
+        hashfunc = imagehash.average_hash
     elif hashmethod == 'phash':
-    	hashfunc = imagehash.phash
+        hashfunc = imagehash.phash
     elif hashmethod == 'dhash':
-    	hashfunc = imagehash.dhash
+        hashfunc = imagehash.dhash
     elif hashmethod == 'whash-haar':
-    	hashfunc = imagehash.whash
+        hashfunc = imagehash.whash
     elif hashmethod == 'whash-db4':
-    	hashfunc = lambda img: imagehash.whash(img, mode='db4')
+        hashfunc = lambda img: imagehash.whash(img, mode='db4')
     else:
-    	usage()
+        usage()
     userpath = sys.argv[2] if len(sys.argv) > 2 else "."
     find_similar_images(userpath=userpath, hashfunc=hashfunc)
     
