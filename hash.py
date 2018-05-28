490746906699649ac134f208dfecb0720de41f17
Zubair <zubair.shokh@gmail.com>
2018-05-28 12:40:08 +0530

    added ahash to wrap

--- hash.py
+++ hash.py
@@ -5,7 +5,7 @@ import six
 
 import imagehash
 
-def generate_hash(image_path, hashsize = 8, hashfunc = imagehash.phash, freqfactor = 4):
+def generate_phash(image_path, hashsize = 8, hashfunc = imagehash.phash, freqfactor = 4):
     hash = hashfunc(Image.open(image_path), hashsize, freqfactor)
     return str(hash)
 
@@ -13,3 +13,8 @@ def hamming_distance(hash1, hash2):
     hash1 = imagehash.hex_to_hash(hash1)
     hash2 = imagehash.hex_to_hash(hash2)
     return str(hash1 - hash2)
+
+def generate_ahash(image_path, hashsize = 8, hashfunc = imagehash.average_hash):
+    hash = hashfunc(Image.open(image_path), hashsize)
+    return str(hash)
+    
