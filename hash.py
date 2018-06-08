ab2e318dc13790c527dc305e7be31618597a40c5
Mangala Kader P <mangala@laava.id>
2018-06-08 16:35:10 +0530

    Added export image option

--- hash.py
+++ hash.py
@@ -5,7 +5,10 @@ import six
 
 import imagehash
 
-def generate_phash(image_path, hashsize = 8, hashfunc = imagehash.phash, freqfactor = 4):
+def generate_phash(image_path, hashsize = 8, hashfunc = imagehash.phash, freqfactor = 4, expose_image = False):
+    if expose_image:
+        export_image(image_path, hashsize, freqfactor)
+    
     hash = hashfunc(Image.open(image_path), hashsize, freqfactor)
     return str(hash)
 
@@ -18,3 +21,16 @@ def generate_ahash(image_path, hashsize = 8, hashfunc = imagehash.average_hash):
     hash = hashfunc(Image.open(image_path), hashsize)
     return str(hash)
     
+def export_image(image_path, hashsize, freqfactor):
+    import os, sys
+    name = os.path.basename(image_path)
+    image = Image.open(image_path)
+    img_size = hashsize * freqfactor
+    image = image.convert("L").resize((img_size, img_size), Image.ANTIALIAS)
+    try:
+        os.mkdir("../hashed_images")
+    except FileExistsError:
+        pass
+    
+    image.save("../hashed_images/%s" % name)
+    return None
